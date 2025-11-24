14f813e3757a5f6a7954383c96c4adcc36dbee31
Ifti-Alamin <iftialmin10@gmail.com>
2025-11-24 11:11:38 +0600

    ready for development with bootstrap

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl font-bold text-red-500 text-center">
-        Hello World
-      </h1>
+      <h1>Hello World</h1>
     </>
   );
 }
